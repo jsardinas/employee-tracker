b882e3448f3b85fc47240018e97fe46f16971a8d
Javier <javier.sardinas@gmail.com>
2022-06-03 13:59:37 -0500

    add mock data

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+insert into department(department_name) values
+('engineering'),
+('not engineering');
+
+insert into roles(title, salary, department_id) values
+('software engineer', 120000, 1),
+('senriot engineer', 160000, 1),
+('staff engineer', 200000, 1),
+('ceo', 300000, 2);
+
+insert into employee(first_name, last_name, role_id, manager_id) values
+('pepe', 'garcia', 3, NULL),
+('jose', 'garcia', 2, 1),
+('pepon', 'garcia', 1, 1),
+('pepin', 'garcia', 1, 1),
+('parasite', 'in chief', 4, NULL);
+
